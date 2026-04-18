435b99a879d33c578d2711976b73ff074527f548
huangxiaoming <251813997@qq.com>
2026-04-18 20:26:42 +0800

    refactor
    优化

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,6 @@
 import { MetadataRoute } from 'next'
 
-const BASE_URL = 'https://sumaitong.com'
+const BASE_URL = 'https://my-sumaitong-website.vercel.app'
 
 export default function robots(): MetadataRoute.Robots {
   return {
